c28e6fe5f5dc488739eefe76d737f9f8df48da7f
gwongwen <gwongwen.trash@gmail.com>
2024-02-05 16:23:03 +0100

    simplify code to only test ADC and EEPROM

--- src/main.c
+++ src/main.c
@@ -12,11 +12,9 @@
 #include <stdio.h>
 
 #include "app_rom.h"
-#include "app_rtc.h"
 
 void adc_work_handler(struct k_work *work_adc)
 {
-	const struct device *rtc_dev = NULL;
 	const struct device *rom_dev = NULL;
 
 	uint16_t raw_val = app_adc_handler();
@@ -25,7 +23,6 @@ void adc_work_handler(struct k_work *work_adc)
 
 	printk("ADC handler called\n");
 
-	app_rtc_handler(rtc_dev);
 	payload[0] = raw_val >> 8;
 	payload[1] = raw_val;
 
@@ -44,16 +41,13 @@ K_TIMER_DEFINE(adc_timer, adc_timer_handler, NULL);
 
 int main(void)
 {
-	const struct device *rtc_dev = NULL;
 	const struct device *rom_dev = NULL;
 
-	app_rtc_init(rtc_dev);
 	app_rom_init(rom_dev);
 
 	printk("beginninig of test\n");
 
-	int8_t ret = app_rtc_handler(rtc_dev);
-//	k_timer_start(&adc_timer, K_MSEC(5000), K_MSEC(5000));
+	k_timer_start(&adc_timer, K_MSEC(5000), K_MSEC(5000));
 
 	return 0;
 }
