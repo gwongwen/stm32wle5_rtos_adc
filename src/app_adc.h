333d1ba4a8f26808ce7be69c8fbc0e53e2b1ee8a
gwongwen <gwongwen.trash@gmail.com>
2024-02-06 11:28:55 +0100

    clean header file

--- src/app_adc.h
+++ src/app_adc.h
@@ -8,19 +8,14 @@
 #ifndef APP_SENSOR_H
 #define APP_SENSOR_H
 
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/devicetree.h>
 #include <zephyr/drivers/adc.h>
 
 #define DT_SPEC_AND_COMMA(node_id, prop, idx)   ADC_DT_SPEC_GET_BY_IDX(node_id, idx)
 
 uint16_t app_adc_handler();
+void adc_timer_handler(struct k_timer *adc_dum);
 void adc_work_handler(struct k_work *work_adc);
 
 #endif /* APP_SENSOR_H */
