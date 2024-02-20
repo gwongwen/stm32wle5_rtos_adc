c5c3759f48c9217f5c7727db3c0b2220d0905473
gwongwen <gwongwen.trash@gmail.com>
2024-02-20 14:15:00 +0100

    simplify adc function printk

--- src/app_adc.c
+++ src/app_adc.c
@@ -8,22 +8,21 @@
 #include "app_adc.h"
 
 //  ======== globals ============================================
-uint16_t m_sp_buf;		
+uint16_t sp_buf;		
 
 static const struct adc_dt_spec adc_channels[] = {
 	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, DT_SPEC_AND_COMMA)
 };
 
 struct adc_sequence adc_ch13_seq = {
-	.buffer 		= &m_sp_buf,
-	.buffer_size	= sizeof(m_sp_buf),
+	.buffer 		= &sp_buf,
+	.buffer_size	= sizeof(sp_buf),
 };
 
 //  ======== app_adc_handler ====================================
-uint16_t app_adc_handler(void) {
-
+uint16_t app_adc_handler(void)
+{
 	int8_t err;
-	int32_t val_mv;
 
 	// getting STM32 ADC device at GPIO PB13
 	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
@@ -31,7 +30,7 @@ uint16_t app_adc_handler(void) {
 			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
 		return 0;
 		}
-
+		// getting channel setup
 		err = adc_channel_setup_dt(&adc_channels[i]);
 		if (err < 0) {
 			printk("could not setup channel 13. error: %d\n", err);
@@ -49,7 +48,7 @@ uint16_t app_adc_handler(void) {
 
 		(void)adc_sequence_init_dt(&adc_channels[i], &adc_ch13_seq);
 		err = adc_read(adc_channels[i].dev, &adc_ch13_seq);
-		printk("adc: %d\n", m_sp_buf);
+		printk("adc: %d\n", sp_buf);
 	}
-	return m_sp_buf;
+	return sp_buf;
 }
