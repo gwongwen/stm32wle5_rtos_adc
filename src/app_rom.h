4dfd55805db27acf3b5088bb8c0bc03955d30aaa
gwongwen <gwongwen.trash@gmail.com>
2024-02-20 14:17:56 +0100

    add constant of structure number

--- src/app_rom.h
+++ src/app_rom.h
@@ -15,7 +15,8 @@
 
 //  ======== defines ==============================================
 #define EEPROM_SAMPLE_OFFSET    0
-#define ADC_BUFFER_SYZE         10    // 10 samples for this test
+#define ADC_BUFFER_SYZE         14    // ID+timestamp+value
+#define ADC_STRUCT_SIZE         5     // number of structures to store
 #define LORAWAN_DEV_EUI		    { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x21, 0xA5 }
                                             
 //  ======== prototypes ===========================================
